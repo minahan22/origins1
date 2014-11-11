a6e6e426a859e2d4c0cd5df8cb4a02261c1c922e
minahan <h.mina2010@gmail.com>
2014-11-10 21:38:22 -0800

    Finish log in/log out

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.2.0.beta2'
 gem 'bootstrap-sass',       '3.2.0.0'
 gem 'bcrypt',               '3.1.7'
 
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0.0.beta1'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    debugger
   end
 
  def new
     @user = User.new
   end
+  
  def create
     @user = User.new(user_params)
     if @user.save
+          log_in @user
     	flash[:success] = "Welcome to the Sample App!"
     	      redirect_to @user
       # Handle a successful save.
